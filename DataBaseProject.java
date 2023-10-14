c2b94a73ee7f9d724fd78e5825b57fbaef0cd1cc
Ram <139115511+urstrulyram6@users.noreply.github.com>
2023-10-15 01:08:19 +0530

    Add files via upload

---
+++ DataBaseProject.java
@@ -0,0 +1,13 @@
+package databaseproject;
+
+public class DataBaseProject {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+                Front frnt = new Front();
+                frnt.setVisible(true);
+    }
+    
+}
